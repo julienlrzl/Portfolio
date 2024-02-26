bf6b744917e5d1ba9f70e273ecfd79aa71c87233
julien larzul <julien.larzul@etu.univ-lyon1.fr>
2024-02-26 23:13:16 +0100

    avancement projet navbar

---
+++ portfoliojulien/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <p>test</p>
+    </div>
+  );
+};
+
+export default About;
