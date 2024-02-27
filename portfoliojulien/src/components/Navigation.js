41c5a14fc6e52433e51b7a65a0974ea6810b3f24
julien larzul <julien.larzul@etu.univ-lyon1.fr>
2024-02-27 11:38:02 +0100

    component navbar 90%

--- portfoliojulien/src/components/Navigation.js
+++ portfoliojulien/src/components/Navigation.js
@@ -1,9 +1,22 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
 const Navigation = () => {
+  const [shadow, setShadow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Ajoute une ombre si le défilement est supérieur à 0
+      setShadow(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <nav className="navigation">
+    <nav className={`navigation ${shadow ? "shadow" : ""}`}>
       <div className="navigation-logo">Larzul Julien</div>
       <div className="navigation-links">
         <a href="#home" className="nav-link">
