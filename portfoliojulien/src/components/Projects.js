48062cf0036a89d72f35550e29f63a95d37c52ed
julien larzul <julien.larzul@etu.univ-lyon1.fr>
2024-03-01 14:38:11 +0100

    partie projet terminée sauf pages individuelles

--- portfoliojulien/src/components/Projects.js
+++ portfoliojulien/src/components/Projects.js
@@ -1,69 +1,142 @@
 import React from "react";
+import Folder from "./Folder";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Projects = () => {
   return (
     <div className="main-content">
       <div id="projects" className="container-fluid h-100 d-flex flex-column">
-        <span className="contact-pre-title">What I've built</span>
-        <h1 className="contact-title space">Projects</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
-          expedita iure vero ab veniam autem quo error rem. Eum earum debitis
-          blanditiis sunt eligendi, fuga temporibus, in quos reprehenderit
-          pariatur repellendus aliquam quisquam laudantium magnam exercitationem
-          aliquid nisi consequuntur consequatur sit facere, explicabo porro
-          molestiae natus? Vitae quam asperiores consequatur corporis quia eaque
-          neque perspiciatis dolorem nihil magnam. Mollitia ducimus autem fugit
-          soluta accusantium ipsum incidunt dolorum magnam temporibus, aut saepe
-          natus facilis maiores aspernatur aliquid nisi excepturi neque
-          consequatur deserunt. Saepe quasi, cupiditate reiciendis optio quia
-          sit eos laboriosam tenetur facilis. Recusandae ducimus tempora ipsa
-          aperiam tenetur, animi odio soluta natus voluptate nesciunt amet fuga.
-          Necessitatibus veniam unde hic eos voluptatibus sint magni porro, quo
-          excepturi id doloribus! Reiciendis doloremque dolor ab dolores quasi
-          eius molestiae odio, eos non iusto illo totam esse harum doloribus
-          alias voluptas! Cum corporis obcaecati natus quos deserunt quibusdam
-          quia provident nobis ad, fugiat repellendus quidem nihil adipisci
-          nemo, doloribus accusamus nam placeat doloremque tenetur voluptate.
-          Animi, beatae culpa modi repudiandae fugiat quibusdam eaque tempora
-          quidem nisi dolore assumenda debitis rerum distinctio accusamus, quia
-          obcaecati dolorem ipsa enim inventore quas incidunt id laborum
-          officiis. Eveniet mollitia consequuntur deserunt aliquam illum
-          exercitationem, provident dolorum quos quia sit itaque. Eligendi,
-          natus magnam voluptatibus aspernatur sit nam, veniam harum et
-          inventore similique, ipsum reprehenderit fugiat aut provident dolore
-          voluptatum. Non sed quod rerum unde expedita quaerat dolor eaque totam
-          enim odit dolorum ab iste dolore, obcaecati neque nesciunt vero
-          adipisci quasi optio consequuntur? Odio veritatis reiciendis commodi
-          dolorum rem provident qui ipsa non quibusdam maxime nostrum sint,
-          repudiandae quis enim fugit natus molestias excepturi repellat. Ipsa
-          est magni beatae. Amet officia pariatur temporibus assumenda ullam
-          rerum architecto aliquid veritatis debitis facere esse accusamus
-          doloremque quam recusandae iste ipsam, earum porro et quia beatae
-          minima consequatur nemo hic? Sed, fugit nobis. Illo similique
-          voluptatem eligendi sed tempora nisi magnam officiis culpa optio quasi
-          veniam molestiae, saepe impedit a eaque officia neque odit eius, quas
-          architecto debitis laudantium? Error adipisci illum pariatur similique
-          debitis laboriosam enim ipsa dicta blanditiis odio iusto, quos
-          corporis tempore architecto molestiae nisi, quis itaque, atque est.
-          Fugiat, odit consequatur mollitia, deserunt ipsa consectetur incidunt
-          obcaecati dolor ratione beatae odio, laboriosam eius ut dolore. Sequi
-          incidunt fuga in repellat est debitis quos, sint quis odio. Ratione
-          dolor, sapiente totam quia excepturi molestiae, laborum modi nisi
-          consequuntur voluptates ut quo qui possimus dolorum ea iure provident
-          suscipit consectetur voluptatum dolores animi velit amet. Minima
-          voluptate nulla facilis? Ipsum officiis dolorem accusamus ea eius nemo
-          dignissimos minus animi magnam. Officia omnis quod dolorem laboriosam,
-          ipsam obcaecati tempore, similique unde ut cupiditate reprehenderit
-          nesciunt illo animi vero? Ea illum tempora esse repellendus iusto, ad
-          optio accusantium atque fugit dolor nemo ratione incidunt possimus
-          doloremque ut earum illo voluptatem quam laudantium veritatis aperiam
-          alias quae. Veritatis minima excepturi atque natus nesciunt magnam
-          cupiditate, quas consequatur, dignissimos accusamus dolore. Ea
-          doloremque adipisci assumenda. Nobis libero amet alias reiciendis
-          obcaecati vero debitis delectus natus illum officiis quia, iste magnam
-          repellendus omnis!
-        </p>
+        <span className="contact-pre-title left">What I've built</span>
+        <h1 className="contact-title space left down">Projects</h1>
+        <div className="d-flex flex-column">
+          <div className="responsive">
+            <div
+              className="col-md-4 rectangle-color d-flex align-items-center justify-content-end"
+              style={{ textAlign: "right" }}
+            >
+              <div className="d-flex flex-row align-items-center">
+                <div>
+                  <Folder />
+                </div>
+                <div>
+                  <a
+                    href="https://github.com/julienlrzl"
+                    className="github-projects"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                </div>
+              </div>
+              <div className="d-flex flex-column">
+                <h1 className="title-project">Portfolio 💼</h1>
+                <p className="text-project">
+                  Creation of a personal portfolio website intended to showcase
+                  my skills and completed projects.
+                </p>
+                <span className="footer-project">React.js SCSS</span>
+              </div>
+            </div>
+            <div className="col-md-4 text-center rectangle-color">
+              <div className="d-flex flex-row align-items-center">
+                <div>
+                  <Folder />
+                </div>
+                <div>
+                  <a
+                    href="https://github.com/julienlrzl"
+                    className="github-projects"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                </div>
+              </div>
+              <div className="d-flex flex-column">
+                <h1 className="title-project">LaBoutiqueFrançaise 🍎</h1>
+                <p className="text-project">
+                  A website featuring an e-commerce platform for selling food,
+                  complete with authentication capabilities for users.
+                </p>
+                <span className="footer-project">PHP Twig JavaScript</span>
+              </div>
+            </div>
+            <div className="col-md-4 text-left rectangle-color">
+              <div className="d-flex flex-row align-items-center">
+                <div>
+                  <Folder />
+                </div>
+                <div>
+                  <a
+                    href="https://github.com/julienlrzl"
+                    className="github-projects"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                </div>
+              </div>
+              <div className="d-flex flex-column">
+                <h1 className="title-project">Projet Tutoré 🍿</h1>
+                <p className="text-project">
+                  A website modeled after IMDB, focusing on movie databases and
+                  information.
+                </p>
+                <span className="footer-project">React.js .NET</span>
+              </div>
+            </div>
+          </div>
+          <div className="responsive">
+            <div
+              className="col-md-4 rectangle-color"
+              style={{ textAlign: "right" }}
+            >
+              <div className="d-flex flex-row align-items-center">
+                <div>
+                  <Folder />
+                </div>
+                <div>
+                  <a
+                    href="https://github.com/julienlrzl"
+                    className="github-projects"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                </div>
+              </div>
+              <div className="d-flex flex-column">
+                <h1 className="title-project">Cellular Automata 🧑🏽‍💻</h1>
+                <p className="text-project">
+                  1D cellular automaton in Java and forest fire simulation with
+                  a graphical interface.
+                </p>
+                <span className="footer-project">Java</span>
+              </div>
+            </div>
+            <div className="col-md-4 text-center rectangle-color">
+              <div className="d-flex flex-row align-items-center">
+                <div>
+                  <Folder />
+                </div>
+                <div>
+                  <a
+                    href="https://github.com/julienlrzl"
+                    className="github-projects"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                </div>
+              </div>
+              <div className="d-flex flex-column">
+                <h1 className="title-project">Connect 4 🎲</h1>
+                <p className="text-project">
+                  Graphical Interface Implementation: Mastering Object-Oriented
+                  Programming through Game Development
+                </p>
+                <span className="footer-project">Java</span>
+              </div>
+            </div>
+            <div className="col-md-4 text-left rectangle-color-not"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
